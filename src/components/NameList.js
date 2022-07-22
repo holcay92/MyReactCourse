ade19c4873f1055682dc82a80bb651fdb0a6c01a
Halil Olcay <holcay92@gmail.com>
2022-07-22 15:28:11 +0300

    list rendering with map function personlist and person are created as two different component, so each has only one task

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import Person from './Person'
 
 //list rendering with map function
 function NameList() {
-    const names =['Bruce','Clark','Diana']
-    const nameList = names.map(name => <h2>{name}</h2>)
-  return (
-    <div>
-       {
-        nameList
-       }
-    </div>
-  )
+    const persons = [
+        {
+            id: 1,
+            name: 'Halil',
+            age: 30,
+            skill: 'React'
+        },
+        {
+            id: 2,
+            name: 'Setur',
+            age: 40,
+            skill: 'Tour'
+        },
+        {
+            id: 3,
+            name: 'Koc',
+            age: 60,
+            skill: 'Bussiness'
+        }
+    ]
+    const personList = persons.map(person =>(<Person person = {person}></Person>) )
+    return (
+        <div>
+            {
+                personList
+            }
+        </div>
+    )
 }
 
 export default NameList
