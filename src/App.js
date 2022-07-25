1bd024c712e1c12c1dda5fe0a95894b53cadacd8
Halil Olcay <holcay92@gmail.com>
2022-07-25 13:38:37 +0300

    input handling

--- src/App.js
+++ src/App.js
@@ -17,15 +17,17 @@ import StyleSheet from './components/StyleSheet';
 import Inline from './components/Inline';
 import './appStyles.css'
 import styles from './appStyles.module.css'
+import Form from './components/Form';
 
 class App extends Component {
   render() {
 
     return (
       <div className="App">
-        <h1 className='error'>Error</h1>
-        <h1 className={styles.success}>Success</h1>
-        {/* <Greet name="halil" heroName="Batman">
+       <Form/>
+          {/* <h1 className='error'>Error</h1>
+           <h1 className={styles.success}>Success</h1>
+           <Greet name="halil" heroName="Batman">
             <p>This is children props</p>
             </Greet>
 
